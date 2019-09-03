dc5dbeb3df1889728ccd032bf3d238e7cf2dc3c3
Sierra <horseloverseg@gmail.com>
2019-09-03 15:50:47 -0600

    tried out some unity events

---
+++ Project Experiment 2/Assets/Variables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Variables : MonoBehaviour
+{
+
+	public UnityEvent Event;
+	public float floatValue = 5.5f;
+	public int intValue = 20;
+	public string stringValue = "Bob";
+	public int firePower;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Event.Invoke();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
