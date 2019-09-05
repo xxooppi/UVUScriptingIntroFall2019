070ed985cb2b6061f2d85eaaa485b6928e676a11
Sierra <horseloverseg@gmail.com>
2019-09-04 18:54:42 -0600

    experimented with a mouse click event

---
+++ Events Experiment/Assets/EventClick.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventClick : MonoBehaviour
+{
+
+	public UnityEvent mouseClick;
+
+	private void OnMouseDown()
+	{
+		mouseClick.Invoke();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Events Experiment/Assets/EventCollide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventCollide : MonoBehaviour
+{
+
+	public UnityEvent Event;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Event.Invoke();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
