9f741e03a892e825b1f1051565431d9de0ddf217
Sierra <horseloverseg@gmail.com>
2019-09-10 15:44:36 -0600

    learned the float data function

---
+++ Project 3/Assets/FloatData.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class FloatData : ScriptableObject
+{
+   public float value = 1f;
+
+   public void UpdateValue(float amount)
+   {
+      value += amount;
+   }
+}
---
+++ Project 3/Assets/TriggerEvents.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public UnityEvent triggerEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEvent.Invoke();
+    }
+}
