a9d798a22e01cbbaa34e64d666a9542ca08c1227
Sierra <horseloverseg@gmail.com>
2019-09-13 22:56:42 -0600

    created mouse click event, made really cool light scene

---
+++ My Project 3D/Assets/MouseClick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseClick : MonoBehaviour
+{
+
+    public UnityEvent mouseClick;
+    private void OnMouseDown()
+    {
+        mouseClick.Invoke();
+    }
+}
