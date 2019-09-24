c850a05b4427f9e810f5fa2470ee653219003708
Sierra <horseloverseg@gmail.com>
2019-09-24 15:51:43 -0600

    learned if statements and some rigidbodies

---
+++ LightsOnAndOff/Assets/Scripts/ApplyForce.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+
+public class ApplyForce : MonoBehaviour
+{
+
+	private Rigidbody rb;
+	
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void OnCollisionEnter(UnityEngine.Collision other)
+	{
+		rb.AddForce(0,10f,0);
+	}
+}
+	
+	
---
+++ LightsOnAndOff/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+	public int a = 4;
+	public int b = 5;
+	public int c = 10;
+	public string password = "OU812";
+	public bool lightsOn = true;
+	
+	
+	// Use this for initialization
+	void Start () {
+
+		if (a+b == c)
+		{
+			print(true);
+		}
+
+		if (password == "OU812")
+		{
+			print(true);
+		}
+
+		if (lightsOn)
+		{
+			print(true);
+		}
+	}
+	
+	
+}
