7d6405a54b5dd25ac719ec0e9b2315b621e57f82
Sierra <horseloverseg@gmail.com>
2019-09-30 22:24:49 -0600

    added a scriptable player, not fully working like I wanted though

---
+++ LightsOnAndOff/Assets/Scripts/ScriptablePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ScriptablePlayer : ScriptableObject
+{
+	public GameObject player;
+	public string playerName;
+	public Color playerColor = Color.cyan;
+	public 
+
+	// Use this for initialization
+	void Start ()
+	{
+		var addPlayer = Instantiate(player);
+
+		addPlayer.layer = 0;
+		addPlayer.active = true;
+		addPlayer.transform.position = Vector3.zero;
+		addPlayer.name = playerName;
+		addPlayer.GetComponent<Renderer>().material.color = playerColor;
+	}
+	
+}
