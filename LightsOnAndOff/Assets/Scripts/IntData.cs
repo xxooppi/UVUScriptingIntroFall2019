3a00b5cb1652c4f5ec4972c406532fc64bd8f1da
Sierra <horseloverseg@gmail.com>
2019-10-02 21:46:16 -0600

    added int data script to my lights game

---
+++ LightsOnAndOff/Assets/Scripts/IntData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+
+	public int value = 0;
+
+	public void UpdateValue(int number)
+	{
+		value += number;
+	}
+}
