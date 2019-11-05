309f11e30f71666af9dcdc613b4b61333f3e07e6
Sierra <horseloverseg@gmail.com>
2019-11-05 15:52:09 -0700

    did another version of ai

---
+++ Classwork Redone/Assets/Scripts/AI/AgentHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+[RequireComponent(typeof(NavMeshAgent))]
+public class AgentHandler : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	private Transform currentDestination;
+	private GameObject startObj;
+	public Transform destinationObj;
+	void Start ()
+	{
+		startObj = new GameObject();
+		startObj.transform.position = transform.position;
+		currentDestination = transform;
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		currentDestination = destinationObj;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		currentDestination = startObj.transform;
+	}
+
+	void Update ()
+	{
+		agent.destination = currentDestination.position;
+	}
+}
---
+++ Classwork Redone/Assets/Scripts/MouseEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvents : MonoBehaviour
+{
+	private Camera cam;
+	public Vector3Data data;
+
+	private void Start()
+	{
+		cam = Camera.main;
+	}
+
+	private void OnMouseDown()
+	{
+		RaycastHit hit;
+		var ray = cam.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit))
+		{
+			data.value = hit.point;
+		}
+		Debug.Log(hit);
+	}
+}
