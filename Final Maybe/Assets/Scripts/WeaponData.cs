c0e3fa57ac63588a986e2fa4ae7ced1241921c05
Sierra <horseloverseg@gmail.com>
2019-11-07 23:49:24 -0700

    created weapon prefab and instantiated weapon

--- Final Maybe/Assets/Scripts/WeaponData.cs
+++ Final Maybe/Assets/Scripts/WeaponData.cs
@@ -5,8 +5,16 @@ public class WeaponData : GameArtData
 {
 	public int weaponPower = 10;
 
+	public void InstanceWeapon()
+	{
+		var newWeapon = Instantiate(prefab);
+		var newSprite = newWeapon.GetComponentInChildren<SpriteRenderer>();
+		newSprite.sprite = sprite;
+		newSprite.color = color;
+	}
 	public void OnPurple()
 	{
 		weaponPower = 20;
 	}
+	
 }
