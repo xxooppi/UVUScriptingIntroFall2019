e91a25992a8f6f24cb11d2bf46a1dba262719ec8
Sierra <horseloverseg@gmail.com>
2019-11-14 15:47:01 -0700

    worked with coroutines again

---
+++ ClassWorkReal/Assets/Scripts/FireBedCoroutine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FireBedCoroutine : MonoBehaviour
+{
+    private bool standingOn = false;
+    public UnityEvent takeHealth;
+
+    public void ChangeBool()
+    {
+        standingOn = true;
+    }
+    IEnumerator OnStep()
+    {
+        while (standingOn)
+        {
+            yield return new WaitForSeconds(1f);
+            takeHealth.Invoke();
+        }
+    }
+
+    IEnumerator EndCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+        standingOn = false;
+    }
+}
