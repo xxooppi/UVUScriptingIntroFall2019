249bb5cc61aad185d7b44ed0245095b3ebd77508
Sierra <horseloverseg@gmail.com>
2019-11-19 15:54:45 -0700

    worked with animation

---
+++ ClassWorkReal/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonInput : MonoBehaviour
+{
+	public UnityEvent inputEvent;
+	public string inputWord;
+	void Update ()
+	{
+		if (Input.GetButtonDown(inputWord))
+		{
+			inputEvent.Invoke();
+		}
+	}
+}
