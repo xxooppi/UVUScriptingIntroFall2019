6dd05ebb505fa153f4ab5e93337e41d3a485db65
Sierra <horseloverseg@gmail.com>
2019-12-05 00:58:54 -0700

    tried a couple more things to get follow to work, but no use

---
+++ Final Maybe/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+	private Rigidbody rb;
+	public float speed = 3f;
+	
+	void FixedUpdate ()
+	{
+		Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		rb.AddForce(input*speed);
+	}
+}
--- Final Maybe/Assets/Scripts/Follow.cs
+++ Final Maybe/Assets/Scripts/Follow.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
 public class Follow : MonoBehaviour
 {
 	public Transform destination;
