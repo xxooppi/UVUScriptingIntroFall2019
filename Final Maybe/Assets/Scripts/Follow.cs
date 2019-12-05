ea5e2eb7ef798ea41a17faea306c875fab22bc02
Sierra <horseloverseg@gmail.com>
2019-12-04 23:59:39 -0700

    redid my platforms, deleted old ones, took off a* paths

---
+++ Final Maybe/Assets/Scripts/Follow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+	public Transform destination;
+	public Vector3 destinationDirection;
+	public float speed = 3f;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
