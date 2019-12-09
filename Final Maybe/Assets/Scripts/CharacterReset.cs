3f594f9e6ef05c0357b19bda6c6a0a7971106070
Sierra <horseloverseg@gmail.com>
2019-12-09 02:16:25 -0700

    tried doing a reset script, not work, tried patrolling script, not work

---
+++ Final Maybe/Assets/Scripts/CharacterReset.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterReset : MonoBehaviour
+{
+	public FloatData data;
+	public float delay;
+	public GameObject player;
+	public WaitForSeconds wfsObj;
+	private Transform startPosition;
+	private GameObject startObj;
+	void Start () 
+	{
+		startObj = new GameObject();
+		startObj.transform.position = transform.position;
+		wfsObj = new WaitForSeconds(delay);
+		StartCoroutine(ResetPlayer());
+	}
+
+	IEnumerator ResetPlayer()
+	{
+		if (data.value <= 0)
+		{
+			Destroy(player);
+			yield return wfsObj;
+			data.value = 1f;
+			Instantiate(player);
+			transform.position = startObj.transform.position;
+
+		}
+	}
+}
--- Final Maybe/Assets/Scripts/EnemyPatrol.cs
+++ Final Maybe/Assets/Scripts/EnemyPatrol.cs
@@ -1,14 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.PlayerLoop;
-
+[RequireComponent(typeof(Collider))]
 public class EnemyPatrol : MonoBehaviour
 {
 	public GameObject obj;
-	public Vector3 leftMovement;
-	public Vector3 rightMovement;
-	public float seconds = 0f;
-	public WaitForSeconds hold;
+	public float right;
+	public float left;
+	public float health;
+	public float seconds;
+	public WaitForSeconds wfsObj;
+	private bool enemyMoving = true;
+	private Collider trigger;
+	private Vector3 position;
 	
+	private void Awake()
+	{
+		trigger = GetComponent<Collider>();
+		trigger.isTrigger = true;
+		wfsObj = new WaitForSeconds(seconds);
+		enemyMoving = true;
+		StartCoroutine(EnemyPatrolling());
+	}
+
+	IEnumerator EnemyPatrolling()
+	{
+		while (enemyMoving)
+		{
+			//transform.Translate(Time.deltaTime*right*Vector2.right);
+			position.x = right * Time.deltaTime;
+			yield return wfsObj;
+			//transform.Translate(Time.deltaTime*right*Vector2.left);
+			position.x = left * Time.deltaTime;
+			yield return wfsObj;
+		}
+	}
+
+	private void Update()
+	{
+		if (health <= 0)
+		{
+			enemyMoving = false;
+			Destroy(obj);
+		}
+	}
 }
